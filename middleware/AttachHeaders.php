baa0557aa5a0dc84c87a4f1253fb134a380c53b7
Wilco Menge <wilcomenge@hotmail.com>
2014-10-28 21:09:27 +0100

    Allow authorization headers

--- middleware/AttachHeaders.php
+++ middleware/AttachHeaders.php
@@ -6,7 +6,7 @@ class AttachHeaders extends \Slim\Middleware
 {
     public function call()
     {
-        $this->app->response->headers->set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        $this->app->response->headers->set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         $this->app->response->headers->set('Access-Control-Allow-Origin', $this->app->braindumpConfig['client_cors_domain']);
         
         // Run inner middleware and application
