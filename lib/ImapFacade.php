dddde7d99982c2faff1fed29e6296aaebe5fefd6
Wilco Menge <wilcomenge@hotmail.com>
2015-03-21 12:05:02 +0100

    refactor Imap code into ImapFacade

---
+++ lib/ImapFacade.php
@@ -0,0 +1,79 @@
+<?php namespace Braindump\Api\Lib;
+
+require_once(__DIR__ . '/../model/Sentry/Paris/User.php');
+require_once(__DIR__ . '/../model/Notebook.php');
+require_once(__DIR__ . '/../model/Note.php');
+
+class ImapFacade
+{
+    protected static $default_config = [
+        'server'          => 'imap.yourserver.com',
+        'port'            => 993,
+        'messageLimit'    => 10,
+        'user'            => 'your_imap_user',
+        'password'        => 'your_imap_password',
+        'sourceFolder'    => 'Inbox',
+        'processedFolder' => 'Processed'
+    ];
+
+    private $config;
+
+    private function __construct($config, $server)
+    {
+        $this->config = $config;
+        $this->server = $server;
+    }
+
+    public static function createFacade($config, $server = null)
+    {
+        return new ImapFacade(
+            $config,
+            $server ?: new \Fetch\Server($config['server'], $config['port'])
+        );
+    }
+
+    public function getMessages()
+    {
+        $this->server->setAuthentication($this->config['user'], $this->config['password']);
+        $this->server->setMailbox($this->config['sourceFolder']);
+
+        if ($this->server->numMessages() > $this->config['messageLimit']) {
+            throw new Exception("Too many messages in ".$server->getMailBox());
+        }
+
+        return $this->server->getMessages();
+    }
+
+    public function processMessages($messages)
+    {
+        foreach ($messages as $message) {
+            $this->processMessage($message);
+        }
+    }
+
+    private function processMessage($message)
+    {
+        $sender = (object)$message->getAddresses('sender');
+           
+        $user = \Cartalyst\Sentry\Users\Paris\User::where('email', $sender->address)->find_one();
+        
+        if (!empty($user)) {
+
+            $dataObject = (object)[
+                'title' => $message->getSubject(),
+                'type' => 'HTML',
+                'content' => $message->getMessageBody(true) // HTML will be sanitized in $note->map()
+            ];
+
+            // TODO: Store email folder on user
+            $notebook = \Braindump\Api\Model\Notebook::find_one(2);
+
+            $note = \Braindump\Api\Model\Note::create();
+            $note->map($notebook, $dataObject);
+            $note->user_id = $user->id;
+            $note->save();
+        }
+
+        $message->moveToMailBox($this->config['processedFolder']);
+    }
+}
--- worker/worker.php
+++ worker/worker.php
@@ -1,11 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-require_once __DIR__ . '/../lib/SentryFacade.php';
-require_once __DIR__ . '/../lib/DatabaseFacade.php';
-require_once(__DIR__ . '/../model/Notebook.php');
-require_once(__DIR__ . '/../model/Note.php');
+require_once __DIR__ . '/../lib/ImapFacade.php';
 
 date_default_timezone_set('Europe/Amsterdam');
 
@@ -14,59 +10,18 @@ $worker = new \Kohkimakimoto\Worker\Worker();
 // ... job definitions
 $worker->job('process-mail', ['cron_time' => '* * * * *', 'command' => function() use ($worker) {
 
-    // TODO: Refactor into some IMAPFacade
-
     try {
         // Setup DB connection
         $braindumpConfig = (require __DIR__ . '/../config/braindump-config.php');
         ORM::configure($braindumpConfig['database_config']);
 
-        // Setup Imap connection
+        $imapFacade = \Braindump\Api\Lib\ImapFacade::createFacade($braindumpConfig['imap_config']);
 
-        $config = (object)$braindumpConfig['imap_config'];
-        $server = new \Fetch\Server($config->server, $config->port);
-        $server->setAuthentication($config->user, $config->password);
+        $messages = $imapFacade->getMessages();
 
-        $server->setMailbox($config->sourceFolder);
+        $worker->output->writeln('<comment>'.sizeof($messages).'</comment> Messages found');
 
-        $messageCount = $server->numMessages();
-
-        if ($messageCount > $config->messageLimit) {
-            throw new Exception("Too many messages in ".$server->getMailBox());
-        }
-
-        $worker->output->writeln('<comment>'.$messageCount.'</comment> Messages found in '.$server->getMailBox());
-
-        $messages = $server->getMessages();
-
-        foreach ($messages as $message) {
-            $sender = (object)$message->getAddresses('sender');
-           
-            $user = \Cartalyst\Sentry\Users\Paris\User::where('email', $sender->address)->find_one();
-            
-            if (!$user) {
-                $worker->output->writeln('Message from <comment>'.$sender->address.'</comment> ignored.');
-            } else {
-                $worker->output->writeln('<info>Message found from user <comment>'.$user->email.'</comment>.</info>');
-
-                // TODO: Store email folder on user
-                $notebook = \Braindump\Api\Model\Notebook::find_one(2);
-
-                $note = \Braindump\Api\Model\Note::create();
-
-                $dataObject = (object)[
-                    'title' => $message->getSubject(),
-                    'type' => 'HTML',
-                    'content' => $message->getMessageBody()
-                ];
-
-                $note->map($notebook, $dataObject);
-                $note->user_id = $user->id;
-                $note->save();
-            }
-
-            $message->moveToMailBox($config->processedFolder);
-        }
+        $imapFacade->processMessages($messages);
 
     } catch (Exception $ex) {
         $worker->output->writeln("<fg=red>".$ex->getMessage()."</fg=red>");
