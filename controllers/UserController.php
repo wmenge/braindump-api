4233f0e2b894e19054fb120e79d8de1858f4e25d
Wilco Menge <wilcomenge@gmail.com>
2016-10-30 21:22:38 +0100

    Allow api user to change own password using PUT /user

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -21,5 +21,29 @@ class UserController extends \Braindump\Api\Controller\BaseController {
 
         return outputJson($userArray, $response);
     }
+
+    public function putUser($request, $response) {
+
+        $user = \Sentry::getUser();
+
+        // Get input
+        $input = json_decode($request->getBody());
+
+        $password = htmlentities($request->getParam('password'));
+        $password_confirm = htmlentities($request->getParam('password_confirm'));
+
+        if (empty($password)) {
+            return $response->withStatus(400)->write('No password supplied');
+        }
+
+        // Validate input
+        if ($password != $password_confirm) {
+            return $response->withStatus(400)->write('Passwords do not match');
+        }
+
+        // Only allow password change
+        $user->password = $password;
+        $user->save();
+    }
     
 }
--- routes/user.php
+++ routes/user.php
@@ -5,5 +5,6 @@ require_once(__DIR__ . '/../controllers/UserController.php');
 $app->group('/api', function () {
 
     $this->get('/user[/]', '\Braindump\Api\Controller\User\UserController:getUser');
+    $this->put('/user[/]', '\Braindump\Api\Controller\User\UserController:putUser');
 
 })->add('Braindump\Api\Admin\Middleware\apiAuthorize')->add('Braindump\Api\Admin\Middleware\apiAuthenticate');
